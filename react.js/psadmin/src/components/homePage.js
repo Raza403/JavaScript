a8862a9dcfc2379fcc23ab169e634fd4976e191c
Ahmed Raza <raza403@gmail.com>
2017-10-07 14:21:09 +0500

    Every thing working created two pages with static header

--- react.js/psadmin/src/components/homePage.js
+++ react.js/psadmin/src/components/homePage.js
@@ -1,14 +1,16 @@
-'use strict';
+"use strict";
+
 var React = require('react');
+
 var Home = React.createClass({
-    render: function(){
-        return (
-                <div className = "jumbotron">
-                    <h1>Plural sight Admin</h1>
-                    <p>I am learning react, this is not my first component of react but first for this course</p>
-                </div>
-        );
-    }
+	render: function() {
+		return (
+			<div className="jumbotron">
+				<h1>Pluralsight Administration</h1>
+				<p>React, React Router, and Flux for ultra-responsive web apps.</p>
+			</div>
+		);
+	}
 });
 
 module.exports = Home;
