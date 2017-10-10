474858be1950acc917afca4eedb87bd688834e11
Ahmed Raza <raza403@gmail.com>
2017-10-10 17:40:17 +0500

    started working

--- es-6/1st.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-let productId = 12;
-{
-    let productId = 2000;
-    console.log('inside block',productId);
-}
-console.log(productId); 
-
-
--- es-6/spreadReast.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var showCAt = (productId, ...cats) => {
-    console.log(cats instanceof Array);
-}
-
-showCAt(123,'search',' advertising');
