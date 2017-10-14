ea6738cbbb48e61f1602be1574e60a860925feda
Ahmed Raza <raza403@gmail.com>
2017-10-14 15:34:24 +0500

    Added nav bar using react router

--- react.js/react-redux/src/components/App.js
+++ react.js/react-redux/src/components/App.js
@@ -1,11 +1,11 @@
 // This component handles the app tempelate used on every page.
 import React, {PropTypes} from 'react';
-
+import Header from './common/Header';
 class App extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-                <p>Header Here...</p>
+                <Header />
                 {this.props.children}
             </div>
         );
