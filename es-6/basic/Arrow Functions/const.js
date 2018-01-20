45f5052608eb8161dbcc1a77d0d00a0820e8613c
Ahmed Raza <raza403@gmail.com>
2018-01-20 12:01:22 +0500

    Added different uses of loops

--- es-6/basic/Arrow Functions/const.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const mark = 100;
-if (mark > 0)
-{
-    const mark = 10;
-    console.log('inside: ',mark);
-}
-console.log('outside: ',mark);
--- es-6/basic/Arrow Functions/old.js
+++
@@ -1,6 +0,0 @@
-let updateFunctions = [];
-for (var i=0;i<2;i++)
-{
-    updateFunctions.push(function(){return i;});
-}
-console.log(updateFunctions[0]());
