20754b8b53e95778741661446775cf13a38b1608
Ahmed Raza <raza403@gmail.com>
2018-01-21 20:17:44 +0500

    Added eslint

--- AJAX Type Ahead/script.js
+++ AJAX Type Ahead/script.js
@@ -2,39 +2,39 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
 const cities = [];
 fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => cities.push(...data));
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
 
 function findMatches(wordToMatch, cities) {
-    return cities.filter(place => {
-        // here we need to figure out if the city or state matches what was searched
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex)
-    });
+  return cities.filter(place => {
+    // here we need to figure out if the city or state matches what was searched
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+  });
 }
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function displayMatches() {
-    if (this.value == "") {
-suggestions.innerHTML =  "<li>Filter for a city</li><li>or a state</li>"
-    } else {
-        const matchArray = findMatches(this.value, cities);
-        const html = matchArray.map(place => {
-            const regex = new RegExp(this.value, 'gi');
-            const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-            const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-            return `
+  if (this.value == "") {
+    suggestions.innerHTML = "<li>Filter for a city</li><li>or a state</li>"
+  } else {
+    const matchArray = findMatches(this.value, cities);
+    const html = matchArray.map(place => {
+      const regex = new RegExp(this.value, 'gi');
+      const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+      const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+      return `
       <li>
         <span class="name">${cityName}, ${stateName}</span>
         <span class="population">${numberWithCommas(place.population)}</span>
       </li>
     `;
-        }).join('');
-        suggestions.innerHTML = html;
-    }
+    }).join('');
+    suggestions.innerHTML = html;
+  }
 }
 
 const searchInput = document.querySelector('.search');
