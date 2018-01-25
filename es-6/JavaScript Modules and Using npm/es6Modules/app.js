ad48ed359cc24fa97243af428ce6cfc95139a852
Ahmed Raza <raza403@gmail.com>
2018-01-25 22:43:10 +0500

    Added config file to export modules

--- es-6/JavaScript Modules and Using npm/es6Modules/app.js
+++ es-6/JavaScript Modules and Using npm/es6Modules/app.js
@@ -1,5 +1,13 @@
 import {uniq} from 'lodash';
 import insane from 'insane';
+import {apiKey as key,
+  url,
+  sayHi,
+  old,
+  dog} from "./config";
 
 const ages = [1,21,2,4,5,3,1,2,4,5,3];
 console.log(uniq(ages));
+console.log(key,old,dog);
+console.log(url);
+sayHi('ahmed');
