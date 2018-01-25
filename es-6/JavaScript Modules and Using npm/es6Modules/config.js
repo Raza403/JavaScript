456a184faaab7314a1951837f38af54335237427
Ahmed Raza <raza403@gmail.com>
2018-01-25 22:54:41 +0500

    Update

---
+++ es-6/JavaScript Modules and Using npm/es6Modules/config.js
@@ -0,0 +1,11 @@
+export const apiKey = 'abc123';
+export const url = 'www.devbros.tk';
+
+export function sayHi (name){
+  console.log(`Hello ${name}`);
+};
+
+const age = 30;
+const dog = 'mirza';
+
+export{age as old,dog};
