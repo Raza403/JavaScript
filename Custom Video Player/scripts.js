2adbda687ed4268d75345a6210e8bcf5e2785b13
Ahmed Raza <raza403@gmail.com>
2018-02-02 21:20:36 +0500

    Work complete on custom video player

--- Custom Video Player/scripts.js
+++ Custom Video Player/scripts.js
@@ -26,13 +26,32 @@ function skip(){
   video.currentTime += parseFloat(this.dataset.skip);
 }
 function handleRangeUpdate(){
-    
+    video[this.name] = this.value;
+}
+
+function handleProgress(){
+  const percentage = (video.currentTime/video.duration) * 100;
+  progressBar.style.flexBasis = `${percentage}%`;
+}
+
+function scrub(e){
+  // (Clicked place/total width of progress bar) * video duration
+const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+video.currentTime = scrubTime;
 }
 
 // Hook up the event listeners  
 video.addEventListener("click",togglePlay);
 video.addEventListener("play",updateButton);
 video.addEventListener("pause",updateButton);
+video.addEventListener("timeupdate",handleProgress);
 toggle.addEventListener("click",togglePlay);
 skipButtons.forEach(button => button.addEventListener("click",skip));
-ranges.forEach(range => range.addEventListener("mousedown",handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("change",handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove",handleRangeUpdate));
+let mousedown = false;
+progress.addEventListener("click",scrub);
+//First it will check if mouse is down then run scrub function
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown",() => mousedown = true);
+progress.addEventListener("mouseup",() => mousedown = false);
