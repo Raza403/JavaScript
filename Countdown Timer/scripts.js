16b882e8c0f2797b34114509d2a33002a55ed9aa
Ahmed Raza <raza403@gmail.com>
2018-02-24 12:43:24 +0500

    Work started again on countdown timer

--- Countdown Timer/scripts.js
+++ Countdown Timer/scripts.js
@@ -1,23 +1,13 @@
-let countdown;
-const displayTime = document.querySelector('.display__time-left');
-function timer (seconds){
-     const now = Date.now();
-     const then = now + seconds*1000;
-     displayTimeLeft(seconds);
-    countDown = setInterval(()=> {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
-        //Check if we should stop it
-        if (secondsLeft < 0){
-            clearInterval(countDown);
-            return;
-        }
-        displayTimeLeft(secondsLeft);
-     },1000)
-}
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  
+  setInterval(()=>{
+    const timeLeft = Math.round((then - Date.now()) / 1000);
+    if (timeLeft <= 0){
+      
+    }
+    console.log(timeLeft);
+  },1000);
 
-function displayTimeLeft(seconds){
-    const mins = Math.floor(seconds/60)
-    const secs = seconds%60;
-    displayTime.textContent =`${mins}:${secs < 10 ? '0' : ''}${secs}`;
-    document.title = Display;
 }
